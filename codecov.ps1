f235d8ad9fb4b34c69cdefe8c82dae3ecb83f464
Damian Kras <damiankras@o2.pl>
2016-10-02 22:00:09 +0200

    Add cert and some code to codecov.ps1

--- codecov.ps1
+++ codecov.ps1
@@ -6,7 +6,6 @@ $Configuration = "AppVeyor"
 # #local
 $projectPath = "D:\dev\Visual Studio 2015\Projects\smoksmog-windows"
 $pythonPath= "C:\Program Files (x86)\Python35-32"
-#$Configuration = "Debug"
 
 # Setup Python and get codecov script
 $pythonScripts = "$pythonPath\Scripts"
@@ -40,7 +39,7 @@ foreach ($testProject in $testProjects) {
 	$testContainer = """$testProject\$outputPath\$assemblyName.dll"""
 	if($outputType -ne "Library"){
 		continue
-		$testContainer = """$testProject\$outputPath\$assemblyName.exe"""
+		#$testContainer = """$testProject\$outputPath\$assemblyName.exe"""
 	}
 
 	# Manual run MSTest
@@ -61,3 +60,31 @@ foreach ($testProject in $testProjects) {
 
 	& "$pythonScripts\codecov" -f $outputCoverage -X gcov 2> $null
 }
+
+#$AppPackage = "$projectPath\SmokSmog.Core.WinRT.Tests\AppPackages\SmokSmog.Core.WinRT.Tests_1.0.0.0_x86_AppVeyor_Test"
+#Certutil -addstore root "$AppPackage\SmokSmog.Core.WinRT.Tests_1.0.0.0_x86_AppVeyor.cer"
+##Manually run vsTest
+## & $vsTest /Platform:x86 /InIsolation "$AppPackage\SmokSmog.Core.WinRT.Tests_1.0.0.0_x86_AppVeyor.appx"
+
+##-targetargs:"/Logger:trx /inIsolation /Platform:x64 unittest.dll" /resultsfile:"unittest.trx"
+##-filter:"+[*]*"
+
+#& $openCoverConsole `
+#	-register:user `
+#	-output:"""$AppPackage\coverage.xml""" `
+#	"-filter:+[SmokSmog*]* -[*Tests]* -[*TestFramework*]* -[*]Microsoft.ApplicationInsights.Extensibility.Implementation.External*" `
+#	"-excludebyattribute:*.ExcludeFromCodeCoverage*" `
+#	-hideskipped:All `
+#	-target:$vsTest `
+#	-targetargs:"/Logger:trx /Platform:x86 /InIsolation """"$AppPackage\SmokSmog.Core.WinRT.Tests_1.0.0.0_x86_AppVeyor.appx""" `
+#	-mergebyhash
+#	 #/resultsfile:"""$AppPackage/unittest.trx""" `
+
+## & "$pythonScripts\codecov" -f """$AppPackage\coverage.xml""" -X gcov 2> $null
+
+##..\packages\OpenCover.4.6.519\tools\OpenCover.Console.exe -register:user
+##  "-target:C:\Program Files (x86)\Microsoft Visual Studio 14.0\Common7\IDE\CommonExtensions\Microsoft\TestWindow\vstest.console.exe"
+
+##  "-targetargs:..\a\Release\Src\DependencyCollector\Net40.Tests\Microsoft.ApplicationInsights.DependencyCollector.Net40.Tests.dll  [CUT: all assemblies]
+##  ..\a\Release\Src\WindowsServer\WindowsServer.Nuget.Tests\WindowsServer.Nuget.Tests.dll /TestCaseFilter:TestCategory!=Required_4_5_1 /logger:trx"
+##  "-filter:+[Microsoft.AI*]* -[*Tests]* -[*TestFramework*]* -[*]Microsoft.ApplicationInsights.Extensibility.Implementation.External*" -hideskipped:All -output:.\coverage.xml
