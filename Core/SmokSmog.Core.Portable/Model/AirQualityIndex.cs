fee0d2548a6a97adc8b60a0051f66d5b1b4b615d
Damian Kras <damiankras@o2.pl>
2017-01-15 20:16:13 +0100

    Station Page design

--- Core/SmokSmog.Core.Portable/Model/AirQualityIndex.cs
+++ Core/SmokSmog.Core.Portable/Model/AirQualityIndex.cs
@@ -39,8 +39,8 @@
             {
                 switch (Level)
                 {
-                    case AirQualityLevel.VeryGood: return "#FF92D050";
-                    case AirQualityLevel.Good: return "#FF00b050";
+                    case AirQualityLevel.VeryGood: return "#FF00b050";
+                    case AirQualityLevel.Good: return "#FF92D050";
                     case AirQualityLevel.Moderate: return "#FFFFFF00";
                     case AirQualityLevel.Sufficient: return "#FFFFC000";
                     case AirQualityLevel.Bad: return "#FFFF0000";
@@ -59,17 +59,20 @@
             if (!parameterValue.HasValue)
                 return new AirQualityIndex();
 
-            double index = parameterValue.Value;
+            double index = parameterValue.Value * 5;
             switch (parameterType)
             {
-                case ParameterType.SO2: index /= 350d; break;
-                case ParameterType.NO2: index /= 200d; break;
-                case ParameterType.CO: index /= 10000d; break;
-                case ParameterType.O3: index /= 120d; break;
-                case ParameterType.PM10: index /= 100d; break;
-                case ParameterType.C6H6: index /= 40d; break;
+                // Polish Air Quality Index based on WIOŚ algorithm helpful links :
+                // http://aqicn.org/faq/2015-09-03/air-quality-scale-in-poland/pl/ http://monitoring.krakow.pios.gov.pl/
 
-                case ParameterType.PM25:
+                case ParameterType.SO2: index /= 350d; break; // Confirmed
+                case ParameterType.NO2: index /= 200d; break; // Confirmed
+                case ParameterType.CO: index /= 10000d; break; // Most Probably
+                case ParameterType.O3: index /= 120d; break; // Confirmed
+                case ParameterType.PM10: index /= 100d; break; // Confirmed
+                case ParameterType.C6H6: index /= 40d; break; // Confirmed
+                case ParameterType.PM25: index /= 60d; break; // Confirmed
+
                 case ParameterType.NOx:
                 case ParameterType.NO:
                 case ParameterType.UNKNOWN:
