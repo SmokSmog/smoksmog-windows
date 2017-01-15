6db7289049ab4fc62da1444a93e538f383bf1ccc
Damian Kras <damiankras@o2.pl>
2017-01-15 11:55:50 +0100

    Build version 2.0.{build}.{branch} + AssetsRestore targetsize

--- AssetsRestore.ps1
+++ AssetsRestore.ps1
@@ -1,8 +1,8 @@
-
 $win81 = [pscustomobject]@{
 	Name = "win81"
 	Folder = "./App/SmokSmog.Windows/"
 	Scales = 180,140,100,80
+	Target = 16,24,32,48,256
 	Assets = 7,9,10,11,2,6,1,4
 }
 
@@ -10,6 +10,7 @@ $wp81 = [pscustomobject]@{
 	Name = "wp81"
 	Folder = "./App/SmokSmog.WindowsPhone/"
 	Scales = 240,140,100
+	Target = 0
 	Assets = 8,9,10,3,6,1,5
 }
 
@@ -17,6 +18,7 @@ $uwp = [pscustomobject]@{
 	Name = "uwp"
 	Folder = "./App/SmokSmog.WindowsUniversal/"
 	Scales = 400,200,150,125,100
+	Target = 16,24,32,48,256
 	Assets = 8,9,10,11,3,6,1,4
 }
 
@@ -37,15 +39,15 @@ $filesNames =@{
 
 $folders = "Badge", "Icon", "SplashScreen", "Store", "Tiles"
 $tiles = "Small","Medium","Wide","Large"
+$sharedPath = "./App/SmokSmog.Shared/"
 
 foreach ($project in $projects) {
-
 	$path = "$($project.Folder)"
 
 	foreach ($folder in $folders) {
 		New-Item -ItemType Directory -Force -Path "$path/Assets/$folder"
 	}
-	
+
 	foreach ($folder in $tiles) {
 		New-Item -ItemType Directory -Force -Path "$path/Assets/Tiles/$folder"
 	}
@@ -55,7 +57,26 @@ foreach ($project in $projects) {
 		foreach ($scale in $project.Scales){
 			$assetPath = "$assetName.scale-$scale.png"
 
-			Copy-Item "./App/SmokSmog.Shared/$assetPath" "$path/$assetPath" -force
+			If (Test-Path "$sharedPath/$assetPath"){
+				# // File exists
+				Copy-Item "$sharedPath/$assetPath" "$path/$assetPath" -force
+			}
+		}
+
+		foreach ($target in $project.Target){
+			$assetTargetSrcPath = "$assetName.targetsize-$target.png"
+			$assetTargetAltPath = "$assetName.targetsize-$($target)_altform-unplated.png"
+			
+			#Square44x44Logo.targetsize-256_altform-unplated
+
+			If (Test-Path "$sharedPath/$assetTargetSrcPath"){
+				# // File exists
+				Copy-Item "$sharedPath/$assetTargetSrcPath" "$path/$assetTargetSrcPath" -force
+			}
+			If (Test-Path "$sharedPath/$assetTargetAltPath"){
+				# // File exists
+				Copy-Item "$sharedPath/$assetTargetAltPath" "$path/$assetTargetAltPath" -force
+			}
 		}
 	}
 }
