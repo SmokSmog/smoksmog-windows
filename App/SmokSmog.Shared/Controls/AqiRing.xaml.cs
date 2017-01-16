704647092138bb71631f7e6b3ac966c62ce93339
Damian Kras <damiankras@o2.pl>
2017-01-17 00:18:01 +0100

    Styling for Station Page, make AqiRing updates

--- App/SmokSmog.Shared/Controls/AqiRing.xaml.cs
+++ App/SmokSmog.Shared/Controls/AqiRing.xaml.cs
@@ -18,16 +18,50 @@ namespace SmokSmog.Controls
         }
 
         public static readonly DependencyProperty EndAngleProperty =
-            DependencyProperty.Register("EndAngle", typeof(double), typeof(AqiRing), new PropertyMetadata(-50));
+            DependencyProperty.Register("EndAngle", typeof(double), typeof(AqiRing), new PropertyMetadata(-150));
 
         public AirQualityIndex AQI
 
         {
             get { return (AirQualityIndex)GetValue(AQIProperty); }
-            set { SetValue(AQIProperty, value); }
+            set { SetValue(AQIProperty, value ?? AirQualityIndex.Unavaible); }
         }
 
         public static readonly DependencyProperty AQIProperty =
-            DependencyProperty.Register("AQI", typeof(AirQualityIndex), typeof(AqiRing), new PropertyMetadata(AirQualityIndex.Unavaible));
+            DependencyProperty.Register("AQI", typeof(AirQualityIndex), typeof(AqiRing), new PropertyMetadata(AirQualityIndex.Unavaible, AQIChanged));
+
+        private static void AQIChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+            var ring = d as AqiRing;
+            if (ring != null)
+            {
+                if (e.NewValue == null)
+                {
+                    ring.AQI = AirQualityIndex.Unavaible;
+                    return;
+                }
+
+                var aqi = ring.AQI;
+
+                if (aqi.Value.HasValue)
+                {
+                    double val = 0.0;
+                    if (aqi.Value > 0 && aqi.Value < 10)
+                    {
+                        ring.EndAngle = aqi.Value.Value / 10d * 300 - 150;
+                    }
+                    else if (aqi.Value >= 10)
+                    {
+                        ring.EndAngle = 150;
+                    }
+                    else
+                        ring.EndAngle = -150;
+                }
+                else
+                {
+                    ring.EndAngle = -150;
+                }
+            }
+        }
     }
 }
