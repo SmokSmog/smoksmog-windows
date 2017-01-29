cf6319fa1629b05701a88303fef4a71258b90db5
Damian Kras <damiankras@o2.pl>
2017-01-29 23:48:36 +0100

    Update Packages, add dependency for UWP.Notification, Start working on Tiles

---
+++ App/SmokSmog.Shared/Views/NotificationPage.xaml.cs
@@ -0,0 +1,14 @@
+﻿using SmokSmog.Navigation;
+using Windows.UI.Xaml.Controls;
+
+namespace SmokSmog.Views
+{
+    [Navigation(ContentType = ContentType.Main)]
+    public sealed partial class NotificationPage : Page
+    {
+        public NotificationPage()
+        {
+            this.InitializeComponent();
+        }
+    }
+}
